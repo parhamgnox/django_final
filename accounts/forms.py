520af638e0dbc22e72e3b7ac38839ef0609af293
parhamgnox <137355548+parhamgnox@users.noreply.github.com>
2023-10-17 22:11:33 +0330

    final

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,7 +12,7 @@ class CustomUserCreation(UserCreationForm):
 
     class Meta:
         model = CustomeUser
-        fields = ('phone_number', 'password1', 'password2')
+        fields = ('id_code', 'password1', 'password2')
 
 
 class CaptchaForm(forms.Form):
--- accounts/models.py
+++ accounts/models.py
@@ -1,29 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, AbstractBaseUser,BaseUserManager,PermissionsMixin
 
-import phonenumbers
 
 class CustomeBaseUserManager(BaseUserManager):
-    def create_user(self, phone_number, password=None, **extra_fields):
-        if not phone_number:
-            raise ValueError('The phone number field must be set')
+    def normalize_id(self, id_code):
+        id_code_str = str(id_code)
+        if not id_code_str.isalnum() or len(id_code_str) != 10:
+            raise ValueError('The given id is wrong')
+        return id_code_str
 
-        # Normalize the phone number
-        normalized_phone_number = phonenumbers.parse(phone_number, None)
-        if not phonenumbers.is_valid_number(normalized_phone_number):
-            raise ValueError('Invalid phone number')
 
-        normalized_phone_number = phonenumbers.format_number(normalized_phone_number, phonenumbers.PhoneNumberFormat.E164)
-
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-
-        user = self.model(phone_number=normalized_phone_number, **extra_fields)
+    def create_user(self, id_code, password, **extra_fields):
+        id_code = self.normalize_id(id_code)
+        user = self.model(id_code=id_code, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user
-
-    def create_superuser(self, phone_number, password, **extra_fields):
+    
+    def create_superuser(self, id_code, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -31,23 +25,27 @@ class CustomeBaseUserManager(BaseUserManager):
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(phone_number, password, **extra_fields)
+        return self.create_user(id_code, password, **extra_fields)
+
+
+
+
 
 
 
 class CustomeUser(AbstractBaseUser, PermissionsMixin):
+    id_code = models.IntegerField(unique=True)
     username  = models.CharField(max_length=100, unique=True)
-    phone_number = models.CharField(max_length=15, unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
-    USERNAME_FIELD = 'phone_number'
+    USERNAME_FIELD = 'id_code'
     REQUIRED_FIELDS = []
 
     objects = CustomeBaseUserManager()
 
     def __str__(self):
-        return self.phone_number
+        return self.id_code
