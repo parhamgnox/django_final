84565e8b65638414c8ca47756c92dac617aa984c
parhamgnox <137355548+parhamgnox@users.noreply.github.com>
2023-10-17 21:11:23 +0330

    final

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, AbstractBaseUser,BaseUserManager,PermissionsMixin
 
-
-
-# class CustomeUser(AbstractUser):
-#     id_code = models.CharField(max_length=10,null=True, blank=True)
-#     mobile = models.CharField(max_length=20,null=True, blank=True)
-#     image = models.ImageField(upload_to='users', default='user.jpg')
-
 class CustomeBaseUserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password,  **extra_fields):
         if not email:
             raise ValueError('The given email must be set')
         email = self.normalize_email(email)
@@ -33,6 +26,7 @@ class CustomeBaseUserManager(BaseUserManager):
 class CustomeUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     username  = models.CharField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
